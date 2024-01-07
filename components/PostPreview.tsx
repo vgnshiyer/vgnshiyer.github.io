1db9dfd217840cf78bfb066821a2e3e36a1a5ab2
vgnshiyer <vgnshiyer@gmail.com>
2024-01-07 15:00:23 -0700

    chore: made whole div for post preview clickable

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -3,7 +3,7 @@ import { PostMetadata } from "../types/PostMetadata";
 
 const PostPreview = (props: PostMetadata) => {
     return (
-        <div className="rounded-xl shadow-md bg-semi-light dark:bg-semi-dark flex flex-col md:flex-row items-start">
+        <Link href={`/posts/${props.slug}`} className="rounded-xl shadow-md bg-semi-light dark:bg-semi-dark flex flex-col md:flex-row items-start">
             {props.cover && (
                 <img
                     className="rounded-lg w-full md:w-44 mr-4 object-cover h-48 md:h-full"
@@ -12,13 +12,11 @@ const PostPreview = (props: PostMetadata) => {
                 />
             )}
             <div className="p-6">
-                <Link href={`/posts/${props.slug}`}>
-                    <h2 className="text-xl text-black dark:text-white font-bold hover:underline">{props.title}</h2>
-                </Link>
+                <h2 className="text-xl text-black dark:text-white font-bold hover:underline">{props.title}</h2>
                 <p className="text-md text-tertiary-dark dark:text-tertiary-dark">{props.subtitle}</p>
                 <p className="mt-2 text-sm text-contrast-light dark:text-contrast-dark">{new Date(props.date).toDateString()}</p>
             </div>
-        </div>
+        </Link>
     );
 };
 
