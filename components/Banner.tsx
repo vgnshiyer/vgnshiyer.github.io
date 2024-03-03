8c06b5c8ed606a096d72425327228efe10e8bd21
vgnshiyer <vgnshiyer@gmail.com>
2024-03-02 18:16:20 -0700

    chore: added now link to banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,21 +1,49 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Banner = () => {
-    return (
-        <div className="flex flex-col sm:flex-row justify-between items-center p-0 md:p-4 mb-10">
-            <div className="pl-0 sm:pl-6 md:pl-4">
-                <h1 className="text-2xl sm:text-3xl mb-1 font-bold text-black dark:text-white">Hello 👋</h1>
-                <h1 className="text-4xl sm:text-5xl mb-1 font-bold text-black dark:text-white">I'm <span className="text-contrast-light dark:text-contrast-dark">Vignesh</span></h1>
-                <h1 className="text-4xl sm:text-5xl pl-40 sm:pl-52 font-bold text-contrast-light dark:text-contrast-dark">Iyer</h1>
-                <h2 className="text-xl sm:text-2xl mb-4 font-bold text-black dark:text-white">Software Engineer</h2>
-                <p className="text-tertiary-light dark:text-tertiary-dark text-sm sm:text-base md:text-lg mb-4">Writing code to make people's lives better.</p>
-            </div>
-            <div className="">
-                {/* <img src="/images/banner.png" alt="Vignesh Iyer" className="w-60 sm:w-64 md:w-72 lg:w-80" loading="lazy" /> */}
-                <Image src="/images/banner.png" alt="Vignesh Iyer" width={500} height={500} className="w-60 sm:w-64 md:w-72 lg:w-80" loading="lazy" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="mb-10 flex flex-col items-center justify-between p-0 sm:flex-row md:p-4">
+      <div className="pl-0 sm:pl-6 md:pl-4">
+        <h1 className="mb-1 text-2xl font-bold text-black sm:text-3xl dark:text-white">
+          Hello 👋
+        </h1>
+        <h1 className="mb-1 text-4xl font-bold text-black sm:text-5xl dark:text-white">
+          I'm{" "}
+          <span className="text-contrast-light dark:text-contrast-dark">
+            Vignesh
+          </span>
+        </h1>
+        <h1 className="text-contrast-light dark:text-contrast-dark pl-40 text-4xl font-bold sm:pl-52 sm:text-5xl">
+          Iyer
+        </h1>
+        <h2 className="mb-4 text-xl font-bold text-black sm:text-2xl dark:text-white">
+          Software Engineer
+        </h2>
+        <p className="text-tertiary-light dark:text-tertiary-dark mb-4 text-sm sm:text-base md:text-lg">
+          Writing code to make people's lives better.
+        </p>
+        <br />
+        <Link
+          href="/now"
+          className="text-contrast-light dark:text-contrast-dark text-2xl font-bold hover:underline"
+        >
+          Now »
+        </Link>
+      </div>
+      <div className="">
+        {/* <img src="/images/banner.png" alt="Vignesh Iyer" className="w-60 sm:w-64 md:w-72 lg:w-80" loading="lazy" /> */}
+        <Image
+          src="/images/banner.png"
+          alt="Vignesh Iyer"
+          width={500}
+          height={500}
+          className="w-60 sm:w-64 md:w-72 lg:w-80"
+          loading="lazy"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
